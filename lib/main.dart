09b794c0c42cd8294eab4b836d56a3ce5feb2f6d
phawitpp <phawit.monc@kmutt.ac.th>
2023-04-19 22:08:54 +0700

    [ADD] dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.removeAfter(initialization);
+
   runApp(const MainApp());
+}
+
+Future initialization(BuildContext? context) async {
+  await Future.delayed(const Duration(seconds: 3));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +18,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MultiProvider(
+      providers: [
+        // Add providers here
+      ],
+      child: MaterialApp(
+          title: 'Checkmate',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromRGBO(127, 17, 224, 1),
+              brightness: Brightness.light,
+              // primary:
+              // secondary:
+            ),
+          ),
+          home: Placeholder()),
     );
   }
 }
