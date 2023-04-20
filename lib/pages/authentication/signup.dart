ad310228f55c3680c21109770174305644a9fcd4
phawitpp <phawit.monc@kmutt.ac.th>
2023-04-20 12:14:43 +0700

    [FIX] flutter auth

--- lib/pages/authentication/signup.dart
+++ lib/pages/authentication/signup.dart
@@ -5,6 +5,8 @@ class SignUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Text('Sign Up screen'),
+    );
   }
 }
