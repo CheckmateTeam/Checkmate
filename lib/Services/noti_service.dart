1cb0734b00858513e209c71cb8741669b10a501c
Phatdanai Shinpanjapol <phatdanai.shin@kmutt.ac.th>
2023-05-03 05:02:35 +0700

    [ADD] time set noti (manaul)

--- lib/Services/noti_service.dart
+++ lib/Services/noti_service.dart
@@ -1,9 +1,14 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
 
 class NotificationService {
   String message = "No message.";
 
   static Future<void> initNotification() async {
+    _configureLocalTimeZone();
     FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
         FlutterLocalNotificationsPlugin();
     flutterLocalNotificationsPlugin
@@ -19,7 +24,66 @@ class NotificationService {
     );
 
     await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    
   }
+
+  tz.TZDateTime _convertTime(int hour, int minutes) {
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    tz.TZDateTime scheduleDate = tz.TZDateTime(
+      tz.local,
+      now.year,
+      now.month,
+      now.day,
+      hour,
+      minutes,
+    );
+    if (scheduleDate.isBefore(now)) {
+      scheduleDate = scheduleDate.add(const Duration(days: 1));
+    }
+    return scheduleDate;
+  }
+
+  static Future<void> _configureLocalTimeZone() async {
+    tz.initializeTimeZones();
+    final String timeZone = await FlutterNativeTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZone));
+  }
+
+
+  scheduledNotification({
+    required int hour,
+    required int minutes,
+    required int id,
+    
+  }) async {
+    await FlutterLocalNotificationsPlugin().zonedSchedule(
+      
+      id,
+      'It\'s time to drink water!',
+      'After drinking, touch the cup to confirm',
+      _convertTime(hour, minutes),
+      const NotificationDetails(
+        android: AndroidNotificationDetails(
+          'your channel id',
+          'your channel name',
+          channelDescription: 'your channel description',
+          importance: Importance.max,
+          priority: Priority.high,
+          ticker: 'ticker'
+        ),
+      
+      ),
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.time,
+      payload: 'It could be anything you pass',
+    );
+    
+    
+  }
+
+  
 
   Future<void> sendNotification(
       {required String body, required String title}) async {
@@ -34,4 +98,28 @@ class NotificationService {
     await FlutterLocalNotificationsPlugin()
         .show(0, title, body, notificationDetails);
   }
+
+  cancelAll() async => await FlutterLocalNotificationsPlugin().cancelAll();
+  cancel(id) async => await FlutterLocalNotificationsPlugin().cancel(id);
+
+
+
+
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
